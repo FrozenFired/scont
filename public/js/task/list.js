4f4e664cee914656171e4965c312bbf840c05791
feng <ecom.feng@gmail.com>
2019-01-04 17:31:23 +0100

    V1.1 task-js

--- public/js/task/list.js
+++ public/js/task/list.js
@@ -1,26 +1,4 @@
 $( function() {
-	// 异步传输更改状态
-	$('.changeStatus').click(function(e) {
-		var target = $(e.target)
-		var id = target.data('id')
-		var newStatus = target.data('newstatus')
-		var tr = $('.item-id-' + id)
-		$.ajax({
-			type: 'GET',
-			url: 'taskStatus?id=' + id + '&newStatus=' + newStatus
-		})
-		.done(function(results) {
-			if(results.success === 1) {
-				if(tr.length > 0) {
-					tr.remove()
-				}
-			}
-			if(results.success === 0) {
-				alert(results.failDel)
-			}
-		})
-	})
-
 	// 更新note 隐藏显示表单
 	$('.btnEdit').click(function(e) {
 		var target = $(e.target)
