d6e42bbdaff3e723e982714363d63352756b9a4d
feng <ecom.feng@gmail.com>
2019-04-05 17:01:04 +0200

    V nation list card

--- public/js/delete.js
+++ public/js/delete.js
@@ -3,15 +3,15 @@ $(function() {
 		var target = $(e.target)
 		var urlDel = target.data('urldel')
 		var id = target.data('id')
-		var tr = $('.item-id-' + id)
+		var element = $('.item-id-' + id)
 		$.ajax({
 			type: 'DELETE',
 			url: urlDel + '?id=' + id
 		})
 		.done(function(results) {
 			if(results.success === 1) {
-				if(tr.length > 0) {
-					tr.remove()
+				if(element.length > 0) {
+					element.remove()
 				}
 			}
 			if(results.success === 0) {
