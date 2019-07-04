5b985ec2bdc32d30eb126f0ae57a3d96322b0b2a
feng <ecom.feng@gmail.com>
2019-07-04 14:45:45 +0200

    0.9.21

---
+++ public/js/vder/orderlist.js
@@ -0,0 +1,35 @@
+$( function() {
+	// $(".datepicker").datepicker();
+	// alert(1)
+	$("#unCheck").text($(".unCheck").val())
+	$("#checking").text($(".checking").val())
+	$("#checked").text($(".checked").val())
+
+	$('.btnEdit').click(function(e) {
+		var target = $(e.target)
+		var id = target.data('id')
+		$('.tar-'+id).val($('.ipt-'+id).val())
+		$('.cancel-'+id).show()
+		$('.form-'+id).show()
+		$('.edit-'+id).hide()
+		$('.text-'+id).hide()
+	})
+	$('.btnCancel').click(function(e) {
+		var target = $(e.target)
+		var id = target.data('id')
+		$('.edit-'+id).show()
+		$('.text-'+id).show()
+		$('.cancel-'+id).hide()
+		$('.form-'+id).hide()
+	})
+	$('.showHide').click(function(e) {
+		$('.hideBtn').show()
+		$(this).hide()
+		$(".hideTr").show()
+	})
+	$('.hideBtn').click(function(e) {
+		$('.showHide').show()
+		$(this).hide();
+		$(".hideTr").hide()
+	})
+} );
